c95e1ed0ca04bf4170f921a03e7b32d65f01a910
HuuTuan <huutuan06@gmail.com>
2019-09-17 15:56:58 +0700

    Done Login by Facebook but it must be improve

--- app/src/main/java/com/app/bookselling/view/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/app/bookselling/view/ui/activity/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.app.bookselling.view.ui.activity
 
 import android.content.Intent
+import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 import com.app.bookselling.R
 import com.app.bookselling.app.Application
 import com.app.bookselling.di.module.LoginModule
@@ -12,11 +14,13 @@ import com.facebook.*
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import org.json.JSONException
+import org.json.JSONObject
 import javax.inject.Inject
 
 class LoginActivity : BaseActivity(), View.OnClickListener, LoginView {
 
-    private var btnSignInByFacebook: LoginButton? = null
+    private var btnSignInByFacebook: ImageButton? = null
     private var imgBtnSignInByGoogle: ImageButton? = null
 
     @Inject
@@ -55,29 +59,32 @@ class LoginActivity : BaseActivity(), View.OnClickListener, LoginView {
     }
 
     private fun loginFacebookApp() {
-        mCallbackManager = CallbackManager.Factory.create()
         LoginManager.getInstance().logInWithReadPermissions(
             mActivity,
             listOf(EMAIL, PUBLIC_PROFILE)
         )
-        LoginManager.getInstance().registerCallback(Application.mCallbackManager, object :
-            FacebookCallback<LoginResult> {
-            override fun onSuccess(loginResult: LoginResult) {
-                Log.d("LoginActivity", "Facebook Token: " + loginResult.accessToken.token)
-
+        LoginManager.getInstance().registerCallback(mCallbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                Log.d("LoginActivity", "onSuccess: "+loginResult?.accessToken)
+                Log.d("LoginActivity", "Token: "+loginResult?.accessToken?.token)
+//                val profile: Profile = Profile.getCurrentProfile()
+//                Log.d("LoginActivity", "First name: "+ profile.firstName)
+//                Log.d("LoginActivity", "Last name: "+ profile.lastName)
+                val mGraphRequest: GraphRequest = GraphRequest.newMeRequest(loginResult?.accessToken
+                ) { `object`, response -> }
+                val parameters = Bundle()
+                parameters.putString("fields","id,name,link")
+                mGraphRequest.parameters
+                mGraphRequest.executeAsync()
+                startActivity(Intent(applicationContext, LogoutActivity::class.java))
             }
 
             override fun onCancel() {
-                if (AccessToken.getCurrentAccessToken() != null) {
-                    GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permission/", null, HttpMethod.DELETE, GraphRequest.Callback {
-                        AccessToken.setCurrentAccessToken(null)
-                        LoginManager.getInstance().logOut()
-                    }).executeAsync()
-                }
+                Log.d("LoginActivity", "Login was canceled!")
             }
 
-            override fun onError(exception: FacebookException) {
-                Log.d("LoginActivity", "Facebook onError!")
+            override fun onError(error: FacebookException?) {
+                Log.d("LoginActivity", "Login had error!")
             }
         })
     }
@@ -92,9 +99,4 @@ class LoginActivity : BaseActivity(), View.OnClickListener, LoginView {
 
         mCallbackManager.onActivityResult(requestCode, resultCode, data)
     }
-
-    fun loadUserProfile( newAccessToken: AccessToken) {
-//        var request: GraphRequest = GraphRequest.newMeRequest(newAccessToken,  )
-    }
-
 }
